a223e0f6897aeb3f821a2f111862130e8adb4026
Rahmat Riyadi <rahmat-riyadi@Rahmat-Riyadi-Macbook-Pro.local>
2023-09-19 12:51:57 +0800

    Feat

--- app/Http/Controllers/Public/BlogController.php
+++ app/Http/Controllers/Public/BlogController.php
@@ -12,7 +12,13 @@ use App\Models\Article;
 class BlogController extends Controller
 {
     public function index(){
-        $data = Article::latest()->paginate(4);
+        $category = request()->query('category');
+        $data = Article::
+            when(!is_null($category), function($q) use ($category){
+                $q->where('category_id', $category);
+            })
+            ->latest()
+            ->paginate(4);
         return response()->json([
             'data' => PublicBlogResource::collection($data),
             'meta' => new MetaPaginateResource($data)
